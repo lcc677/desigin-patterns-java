16077c6913f94d43896b823c01d36b0ad73cf228
刘晨晨 <chenchen@starmerx.com>
2021-07-07 09:59:52 +0800

    工厂方法模式和抽象工厂模式

--- src/design/patterns/creator/factory/abstractfactory/pizza/PizzaStore.java
+++ src/design/patterns/creator/factory/abstractfactory/pizza/PizzaStore.java
@@ -4,7 +4,6 @@ package design.patterns.creator.factory.abstractfactory.pizza;
 public abstract class PizzaStore {
     public Pizza orderPizza(String orderType) {
         Pizza pizza = createPizza(orderType);
-        System.out.println(">???????????????????????");
         if(pizza != null) {
             pizza.prepare();
             pizza.bake();
